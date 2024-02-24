928fe2171aa3d467b395e1edf9e47a1d0a74de1b
NhatHM4 <haminhnhat711@gmail.com>
2024-02-24 10:07:29 +0700

    del css

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const path = require('path')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
-
-console.log(path.join(__dirname,'public'))
+// import static file
 app.use(express.static(path.join(__dirname,'public')))
+
+// HTTP logger
 app.use(morgan('combined'))
+
+// change suffix file
 app.engine('hbs', engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'views'));
